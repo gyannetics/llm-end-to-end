0fdafc93b0a84c018d44a086f40f3614582e37fa
Santosh KV <theaimonk24@gmail.com>
2023-12-20 16:29:52 -0600

    updated the python modules for linting

--- chat.py
+++ chat.py
@@ -2,23 +2,23 @@
 Streamlit UI for a Conversational Q&A Chatbot using LangChain and OpenAI.
 """
 
-# import streamlit as st  # Uncomment if dotenv is used
 import streamlit as st
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.chat_models import ChatOpenAI
 
-def get_openai_response(question):
+def get_openai_response(question, chat_model):
     """
     Generates a response from OpenAI to the given question.
 
     Args:
         question (str): The question asked by the user.
+        chat_model: The chat model instance.
 
     Returns:
         str: The AI-generated answer.
     """
     st.session_state['flow_messages'].append(HumanMessage(content=question))
-    answer = chat(st.session_state['flow_messages'])
+    answer = chat_model(st.session_state['flow_messages'])
     st.session_state['flow_messages'].append(AIMessage(content=answer.content))
     return answer.content
 
@@ -27,7 +27,7 @@ st.set_page_config(page_title="Conversational Q&A Chatbot")
 st.header("Let's Chat")
 
 # Initialize the chat model
-chat = ChatOpenAI()
+chat_model = ChatOpenAI()
 
 # Initialize session state for storing messages
 if 'flow_messages' not in st.session_state:
@@ -41,6 +41,6 @@ user_input = st.text_input("Write here:", key="input")
 # Handle button click and display response
 submit = st.button("What do you say?")
 if submit and user_input:
-    response = get_openai_response(user_input)
+    response = get_openai_response(user_input, chat_model)
     st.subheader("Hmmm... \n")
     st.write(response)
--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
+"""
+Setup script for the langchain-demo package.
+"""
+
 from setuptools import setup, find_packages
 
 # Read the contents of your requirements.txt file
-with open('requirements.txt') as f:
+with open('requirements.txt', encoding='utf-8') as f:
     required = f.read().splitlines()
 
 setup(
     name='langchain-demo',
     version='0.1',
     packages=find_packages(),
-    install_requires=required,  # Use the list from requirements.txt here
+    install_requires=required,
     author='Santosh KV',
     author_email='gyannetics@gmail.com',
     description='A short description of your project',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     license='MIT',
     url='https://github.com/gyannetics/llm-end-to-end',
-    # More metadata
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        # Additional classifiers as needed
+    ],
+    keywords='chatbot, langchain, openai',  # Add more relevant keywords
+    # include_package_data=True,
+    # package_data={...},
+    # More metadata as needed
 )
